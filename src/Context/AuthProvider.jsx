3d0f7d97e281666701dfd66c8e8b5c25ce6a4f90
A-Farabi <farabisyl2017@gmail.com>
2025-06-25 23:50:49 +0600

    user auth state capture done

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AuthContext from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import auth from "./../Firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
-  // const [ user, setUser]  = useState(null);
+  const [ user, setUser]  = useState(null);
   const [ loading, setLoading]  = useState(true);
 
   const createUser = (email, password) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
+  useEffect(()=>{
+    const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+      setUser(currentUser)
+      console.log('state captured',currentUser);
+      setLoading(false)
+    })
+
+    return () =>{
+      unsubscribe();
+    }
+
+  }, [])
 
   const authInfo = {
     // user,
